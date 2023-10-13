704a463f3b5e000c4778c23dcc0ad83f2a96168a
suojae3 <suojae3@gmail.com>
2023-10-13 19:19:26 +0900

    Initial Commit

---
+++ FBTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FBTest
+//
+//  Created by ㅣ on 2023/10/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
